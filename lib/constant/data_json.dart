2074f345dda457fc1071afb7c3e2ab91031f4351
mayankmj <81777768+mayankmj@users.noreply.github.com>
2021-11-20 11:23:50 +0530

    Add files via upload

--- lib/constant/data_json.dart
+++ lib/constant/data_json.dart
@@ -1,192 +1,411 @@
- List menuItems = [
-  "Food Items",
-]; 
+List menuItems = [
+  "Main Course",
+  "Snacks",
+  "Beverages",
+  "Deserts",
+];
 
 
 List dataItems = [
-  {
-    "id" : 1,
-    "code" : "SAMOSA",
-    "name" : "Spicy Samosa with red and green chutney",
-    "img" : "https://images.hindustantimes.com/rf/image_size_630x354/HT/p2/2020/08/02/Pictures/_604f7aa4-d439-11ea-bae0-701c4bed6ede.jpg",
-    "price" : 15,
-    "promotionPrice" : 10,
-    "color" : [
-      {
-        "id" : 1,
-        "value" : "https://myfashion2017.s3.amazonaws.com/productimage/Thefashion_20190804141523-034249.JPG"
-      },
-      {
-        "id" : 2,
-        "value" : "https://myfashion2017.s3.amazonaws.com/productimage/Thefashion_20190804141524-980179.JPG"
-      }
-    ],
-    "size" :[
-      {
-        "id" : 1,
-        "value" : "S"
-      },
-      {
-        "id" : 2,
-        "value" : "M"
-      },
-      {
-        "id" : 3,
-        "value" : "L"
-      }
-    ]
-  },
-  {
-    "id" : 2,
-    "code" : "Burger",
-    "name" : "Extra Double Cheese Burger",
-    "img" : "https://static.toiimg.com/photo/79811787.JPG",
-    "price" : 25,
-    "promotionPrice" : 20,
-    "color" : [
-      {
-        "id" : 1,
-        "value" : "https://myfashion2017.s3.amazonaws.com/productimage/Thefashion_20190804150143-329002.JPG"
-      },
-      {
-        "id" : 2,
-        "value" : "https://myfashion2017.s3.amazonaws.com/productimage/Thefashion_20190804150145-295314.JPG"
-      }
-    ],
-    "size" : [
-      {
-        "id" : 1,
-        "value" : "FREE-SIZE"
-      }
-    ]
-  },
-  {
-    "id" : 3,
-    "code" : "Patties",
-    "name" : "Aloo Patties with Paneer",
-    "img" : "https://yourfoodfantasy.com/wp-content/uploads/2016/05/puffed-patties-yourfoodfantasy-com-by-meenu-gupta.jpg",
-    "price" : 25,
-    "promotionPrice" : 20,
-    "color" : [
-      {
-        "id" : 1,
-        "value" : "https://myfashion2017.s3.amazonaws.com/productimage/Thefashion_20190804144248-379817.JPG"
-      },
-      {
-        "id" : 2,
-        "value" : "https://myfashion2017.s3.amazonaws.com/productimage/Thefashion_20190804144250-354241.JPG"
-      }
-    ],
-    "size" : [
-      {
-        "id" : 1,
-        "value" : "S"
-      },
-      {
-        "id" : 2,
-        "value" : "M"
-      },
-      {
-        "id" : 3,
-        "value" : "L"
-      }
-    ]
-  },
-  {
-    "id" : 4,
-    "code" : "Chole Bhature",
-    "name" : "Amritsari Chole with Two Bhature ",
-    "img" : "https://b.zmtcdn.com/data/pictures/8/19557948/5ac58dcc7ee7f7870a6484c07cb61a5e.jpg",
-    "price" : 60,
-    "promotionPrice" : 50,
-    "color" : [
-      {
-        "id" : 1,
-        "value" : "https://myfashion2017.s3.amazonaws.com/productimage/Thefashion_20190804135741-597445.JPG"
-      },
-      {
-        "id" : 2,
-        "value" : "https://myfashion2017.s3.amazonaws.com/productimage/Thefashion_20190804135743-752905.JPG"
-      }
-    ],
+{
+"id" : 1,
+"code" : "Samosa",
+"name" : "Spicy Samosa with red and green chutney",
+"img" : "https://images.hindustantimes.com/rf/image_size_630x354/HT/p2/2020/08/02/Pictures/_604f7aa4-d439-11ea-bae0-701c4bed6ede.jpg",
+"price" : 15,
+"promotionPrice" : 10,
+"color" : [
+{
+"id" : 1,
+"value" : "https://myfashion2017.s3.amazonaws.com/productimage/Thefashion_20190804141523-034249.JPG"
+},
+{
+"id" : 2,
+"value" : "https://myfashion2017.s3.amazonaws.com/productimage/Thefashion_20190804141524-980179.JPG"
+}
+],
+"size" :[
+{
+"id" : 1,
+"value" : "S"
+},
+{
+"id" : 2,
+"value" : "M"
+},
+{
+"id" : 3,
+"value" : "L"
+}
+]
+},
+{
+"id" : 2,
+"code" : "Burger",
+"name" : "Extra Double Cheese Burger",
+"img" : "https://static.toiimg.com/photo/79811787.JPG",
+"price" : 25,
+"promotionPrice" : 20,
+"color" : [
+{
+"id" : 1,
+"value" : "https://myfashion2017.s3.amazonaws.com/productimage/Thefashion_20190804150143-329002.JPG"
+},
+{
+"id" : 2,
+"value" : "https://myfashion2017.s3.amazonaws.com/productimage/Thefashion_20190804150145-295314.JPG"
+}
+],
+"size" : [
+{
+"id" : 1,
+"value" : "FREE-SIZE"
+}
+]
+},
+{
+"id" : 3,
+"code" : "Patties",
+"name" : "Aloo Patties with Paneer",
+"img" : "https://yourfoodfantasy.com/wp-content/uploads/2016/05/puffed-patties-yourfoodfantasy-com-by-meenu-gupta.jpg",
+"price" : 25,
+"promotionPrice" : 20,
+"color" : [
+{
+"id" : 1,
+"value" : "https://myfashion2017.s3.amazonaws.com/productimage/Thefashion_20190804144248-379817.JPG"
+},
+{
+"id" : 2,
+"value" : "https://myfashion2017.s3.amazonaws.com/productimage/Thefashion_20190804144250-354241.JPG"
+}
+],
+"size" : [
+{
+"id" : 1,
+"value" : "S"
+},
+{
+"id" : 2,
+"value" : "M"
+},
+{
+"id" : 3,
+"value" : "L"
+}
+]
+},
+{
+"id" : 4,
+"code" : "Chole Bhature",
+"name" : "Amritsari Chole Bhature ",
+"img" : "https://b.zmtcdn.com/data/pictures/8/19557948/5ac58dcc7ee7f7870a6484c07cb61a5e.jpg",
+"price" : 60,
+"promotionPrice" : 50,
+"color" : [
+{
+"id" : 1,
+"value" : "https://myfashion2017.s3.amazonaws.com/productimage/Thefashion_20190804135741-597445.JPG"
+},
+{
+"id" : 2,
+"value" : "https://myfashion2017.s3.amazonaws.com/productimage/Thefashion_20190804135743-752905.JPG"
+}
+],
 
-    "size" : [
-      {
-        "id" : 1,
-        "value" : "FREE-SIZE"
-      }
-    ]
-  },
+"size" : [
+{
+"id" : 1,
+"value" : "FREE-SIZE"
+}
+]
+},
+{
+"id" : 5,
+"code" : "Chole Kulche",
+"name" : "Masaledaar Chole kulche ",
+"img" : "https://d4t7t8y8xqo0t.cloudfront.net/resized/750X436/eazytrendz%2F2771%2Ftrend20200325141849.jpg",
+"price" : 55,
+"promotionPrice" : 50,
+"color" : [
+{
+"id" : 1,
+"value" : "https://myfashion2017.s3.amazonaws.com/productimage/Thefashion_20170203040458-854541.JPG"
+},
+{
+"id" : 2,
+"value" : "https://myfashion2017.s3.amazonaws.com/productimage/Thefashion_20170203040500-094722.JPG"
+}
+],
+"size" : [
+{
+"id" : 1,
+"value" :"25"
+},
+{
+"id" : 2,
+"value" :"26"
+},
+{
+"id" : 3,
+"value" :"27"
+},
+{
+"id" : 4,
+"value" :"28"
+},
+]
+},
+{
+"id" : 6,
+"code" : "Chowmein",
+"name" : "Vegetrian Chowmein Noodles",
+"img" : "https://www.jocooks.com/wp-content/uploads/2019/03/chow-mein-1-5.jpg",
+"price" : 40,
+"promotionPrice" : 30,
+"color" : [
+{
+"id" : 1,
+"value" : "https://myfashion2017.s3.amazonaws.com/productimage/Thefashion_20170203035456-333499.JPG"
+},
+{
+"id" : 2,
+"value" : "https://myfashion2017.s3.amazonaws.com/productimage/Thefashion_20170203035457-424978.JPG"
+}
+],
+"size" : [
+{
+"id" : 1,
+"value" :"25"
+},
+{
+"id" : 2,
+"value" :"26"
+},
+{
+"id" : 3,
+"value" :"27"
+},
+{
+"id" : 4,
+"value" :"28"
+},
+]
+},
+{
+"id" : 7,
+"code" : "Pizza",
+"name" : "Margherita Pizza",
+"img" : "https://www.wilton.com/dw/image/v2/AAWA_PRD/on/demandware.static/-/Sites-wilton-project-master/default/dw946a4573/images/project/WLRECIP-8829/Mini-Margharita-Pizzas.jpg?sw=800&sh=800.jpeg",
+"price" : 65,
+"promotionPrice" : 60,
+"color" : [
+{
+"id" : 1,
+"value" : "https://myfashion2017.s3.amazonaws.com/productimage/Thefashion_20170203035456-333499.JPG"
+},
+{
+"id" : 2,
+"value" : "https://myfashion2017.s3.amazonaws.com/productimage/Thefashion_20170203035457-424978.JPG"
+}
+],
+"size" : [
+{
+"id" : 1,
+"value" :"25"
+},
+{
+"id" : 2,
+"value" :"26"
+},
+{
+"id" : 3,
+"value" :"27"
+},
+{
+"id" : 4,
+"value" :"28"
+},
+]
+},
+{
+"id" : 8,
+"code" : "Cake",
+"name" : "Choco lava cake",
+"img" : "https://bakerbynature.com/wp-content/uploads/2019/01/moltenchocolatelavacakes-1-of-1.jpg",
+"price" : 40,
+"promotionPrice" : 30,
+"color" : [
+{
+"id" : 1,
+"value" : "https://myfashion2017.s3.amazonaws.com/productimage/Thefashion_20170203035456-333499.JPG"
+},
+{
+"id" : 2,
+"value" : "https://myfashion2017.s3.amazonaws.com/productimage/Thefashion_20170203035457-424978.JPG"
+}
+],
+"size" : [
+{
+"id" : 1,
+"value" :"25"
+},
+{
+"id" : 2,
+"value" :"26"
+},
+{
+"id" : 3,
+"value" :"27"
+},
+{
+"id" : 4,
+"value" :"28"
+},
+]
+},
+{
+"id" : 9,
+"code" : "Rajma Chawal",
+"name" : "Rajma Chawal",
+"img" : "https://foodhistoria.com/wp-content/uploads/2020/04/of-sunday-afternoons-and-rajma-chawal.1024x1024-3-rotated.jpg",
+"price" : 50,
+"promotionPrice" : 50,
+"color" : [
+{
+"id" : 1,
+"value" : "https://myfashion2017.s3.amazonaws.com/productimage/Thefashion_20170203035456-333499.JPG"
+},
+{
+"id" : 2,
+"value" : "https://myfashion2017.s3.amazonaws.com/productimage/Thefashion_20170203035457-424978.JPG"
+}
+],
+"size" : [
+{
+"id" : 1,
+"value" :"25"
+},
+{
+"id" : 2,
+"value" :"26"
+},
+{
+"id" : 3,
+"value" :"27"
+},
+{
+"id" : 4,
+"value" :"28"
+},
+]
+},
+{
+"id" : 10,
+"code" : "Coffe",
+"name" : "Coffe",
+"img" : "https://s3.envato.com/files/271771964/drink-71.jpg",
+"price" : 20,
+"promotionPrice" : 20,
+"color" : [
+{
+"id" : 1,
+"value" : "https://myfashion2017.s3.amazonaws.com/productimage/Thefashion_20170203035456-333499.JPG"
+},
+{
+"id" : 2,
+"value" : "https://myfashion2017.s3.amazonaws.com/productimage/Thefashion_20170203035457-424978.JPG"
+}
+],
+"size" : [
+{
+"id" : 1,
+"value" :"25"
+},
+{
+"id" : 2,
+"value" :"26"
+},
+{
+"id" : 3,
+"value" :"27"
+},
+{
+"id" : 4,
+"value" :"28"
+},
+]
+},
+{
+"id" : 11,
+"code" : "Ice-Cream",
+"name" : "chocolate Ice-Cream",
+"img" : "https://joyfoodsunshine.com/wp-content/uploads/2020/06/homemade-chocolate-ice-cream-recipe-7.jpg",
+"price" : 15,
+"promotionPrice" : 10,
+"color" : [
+{
+"id" : 1,
+"value" : "https://myfashion2017.s3.amazonaws.com/productimage/Thefashion_20170203035456-333499.JPG"
+},
+{
+"id" : 2,
+"value" : "https://myfashion2017.s3.amazonaws.com/productimage/Thefashion_20170203035457-424978.JPG"
+}
+],
+"size" : [
+{
+"id" : 1,
+"value" :"25"
+},
+{
+"id" : 2,
+"value" :"26"
+},
+{
+"id" : 3,
+"value" :"27"
+},
+{
+"id" : 4,
+"value" :"28"
+},
+]
+},
   {
-    "id" : 5,
-    "code" : "Chole Kulche",
-    "name" : "Masaledaar Chole kulche ",
-    "img" : "https://d4t7t8y8xqo0t.cloudfront.net/resized/750X436/eazytrendz%2F2771%2Ftrend20200325141849.jpg",
-    "price" : 55,
-    "promotionPrice" : 50,
-     "color" : [
-      {
-        "id" : 1,
-        "value" : "https://myfashion2017.s3.amazonaws.com/productimage/Thefashion_20170203040458-854541.JPG"
-      },
-      {
-        "id" : 2,
-        "value" : "https://myfashion2017.s3.amazonaws.com/productimage/Thefashion_20170203040500-094722.JPG"
-      }
-      ],
-    "size" : [
-      {
-        "id" : 1,
-        "value" :"25"
-      },
-      {
-        "id" : 2,
-        "value" :"26"
-      },
-      {
-        "id" : 3,
-        "value" :"27"
-      },
-      {
-        "id" : 4,
-        "value" :"28"
-      },
-    ]
-  },
-  {
-    "id" : 6,
-    "code" : "Chowmein",
-    "name" : "Vegetrian Chowmein Noodles",
-    "img" : "https://www.jocooks.com/wp-content/uploads/2019/03/chow-mein-1-5.jpg",
-    "price" : 40,
-    "promotionPrice" : 30,
-    "color" : [
-      {
-        "id" : 1,
-        "value" : "https://myfashion2017.s3.amazonaws.com/productimage/Thefashion_20170203035456-333499.JPG"
-      },
-      {
-        "id" : 2,
-        "value" : "https://myfashion2017.s3.amazonaws.com/productimage/Thefashion_20170203035457-424978.JPG"
-      }
-    ],
-    "size" : [
-      {
-        "id" : 1,
-        "value" :"25"
-      },
-      {
-        "id" : 2,
-        "value" :"26"
-      },
-      {
-        "id" : 3,
-        "value" :"27"
-      },
-      {
-        "id" : 4,
-        "value" :"28"
-      },
-    ]
-  }
+"id" : 12,
+"code" : "Coke",
+"name" : "Coca-Cola",
+"img" : "https://www.eatthis.com/wp-content/uploads/sites/4/media/images/ext/108098914/coca-cola-soda-ice.jpg?quality=82&strip=all&w=1024.jpg",
+"price" : 45,
+"promotionPrice" : 40,
+"color" : [
+{
+"id" : 1,
+"value" : "https://myfashion2017.s3.amazonaws.com/productimage/Thefashion_20170203035456-333499.JPG"
+},
+{
+"id" : 2,
+"value" : "https://myfashion2017.s3.amazonaws.com/productimage/Thefashion_20170203035457-424978.JPG"
+}
+],
+"size" : [
+{
+"id" : 1,
+"value" :"25"
+},
+{
+"id" : 2,
+"value" :"26"
+},
+{
+"id" : 3,
+"value" :"27"
+},
+{
+"id" : 4,
+"value" :"28"
+},
+]
+}
 ];
